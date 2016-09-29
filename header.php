507dfce279709805277a6391d513fbec8b4411de
Josh <jocjohns@mtu.edu>
2016-09-29 01:22:48 -0400

    Added most dependencies.

---
+++ header.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+  <title>MTU Pokemon Go Alert</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="assets/css/bootstrap.min.css" rel="stylesheet" media="screen">
+  <script src="assets/js/jquery-1.11.1.min.js"></script>
+  <script src="assets/js/bootstrap.min.js"></script>
+  <script src="assets/js/app.js"></script>
+</head>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php include "header.php" ?>
+
+<h1 style="text-align: center;">Coming soon!</h1>
+
+<?php include "footer.php" ?>
